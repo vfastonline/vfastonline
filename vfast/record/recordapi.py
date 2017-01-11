d17af129d8984b542b82ca59231434bbf9724d91
sky-du <duminchao>
2017-01-11 16:12:23 +0800

    用户学习记录信息展示, 课程,用户条件  检索

--- vfast/record/recordapi.py
+++ vfast/record/recordapi.py
@@ -1,6 +1,7 @@
 #!encoding:utf-8
 from record.models import Score, WatchRecord
 from vuser.models import User
+from vcourse.models import Video, Course
 from django.db.models import Sum
 
 
@@ -23,3 +24,20 @@ def get_watchtime(model, uid=None):
         result = model.objects.values('user_id').annotate(sum=Sum('video_progress')).all().order_by('-sum')[0:3]
     return result
 
+
+def get_watched_video(uid, cid):
+    """获取用户观看视频信息列表"""
+    course = Course.objects.get(id=cid)
+    videos = Video.objects.filter(course=course).all().values()
+    user = User.objects.get(id=uid)
+    user_watched = WatchRecord.objects.filter(user=user).all().values()
+
+    for video in videos:
+        for watch in user_watched:
+            if video['id'] == watch['id']:
+                # print "用户观看过这个视频", watch['status']
+                video.update({'status': watch['status'], 'video_progress': watch['video_progress']})
+            else:
+                # print '用户没有观看过这个视频'
+                video.update({'status': 'no', 'video_progress': 0})
+    return videos
