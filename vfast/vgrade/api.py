e22808e0d074d278ccd7998afcaf7edf3adc337e
sky-du <duminchao>
2017-02-09 13:57:30 +0800

    vgrade api add functions

--- vfast/vgrade/api.py
+++ vfast/vgrade/api.py
@@ -3,5 +3,11 @@ from vgrade.models import Headimg
 
 #返回所有头像图片的URL
 def headimg_urls():
-    urls = Headimg.objects.filter(type=1).values('url')[0]
+    """返回系统图片保存位置列表
+    [u'/static/head/tuzi.jpg', u'/static/head/shitou.jpg']
+    """
+    urls = []
+    results = Headimg.objects.filter(type=1).values('url')
+    for url in results:
+        urls.append(url['url'])
     return urls
