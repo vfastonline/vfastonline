84e6caeff1b679138cc7f52fed52cb5cd216bce9
sky-du <duminchao>
2017-02-09 14:19:25 +0800

    2017-02-09 14:19

--- vfast/vfast/views.py
+++ vfast/vfast/views.py
@@ -3,9 +3,13 @@ from django.shortcuts import render
 from vgrade.api import headimg_urls
 import random
 from vcourse.models import Program
+from vfast.api import get_id_name
+
 def test(request):
     print 'test'
-    print random.choice(headimg_urls().values())
-    print Program.objects.all()
+    print headimg_urls()
+    # print random.choice(headimg_urls().values())
+    # print Program.objects.all()
+    # print get_id_name(Program)
     return render(request, 'du/testdu.html')
 
