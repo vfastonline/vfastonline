6b1fe16488c490ca0af09834325e1873ba82519e
peng <542429405@qq.com>
2017-02-13 09:35:46 +0800

    Merge branch 'master' of https://github.com/vfastonline/vfastonline.git
    # Conflicts:
    #	vfast/templates/du/badgetest.html
    #	vfast/templates/du/coursetest.html
    #	vfast/templates/du/test.html
    #	vfast/templates/du/usertest.html
    #	vfast/templates/du/video.html
    #	vfast/templates/du/videodu.html

---
+++ vfast/templates/static/js/modalJS.js
@@ -0,0 +1,136 @@
+function register_show(){
+    modal_left.style.width = "39%";
+    modal_right.style.width = "61%";
+    var xmlhttp2 = new XMLHttpRequest();
+    xmlhttp2.open("get","reg_left.html",true);
+    xmlhttp2.onreadystatechange = function(){
+        if(xmlhttp2.readyState == 4 && xmlhttp2.status==200){
+            modal_left.innerHTML = xmlhttp2.responseText;
+            var xmlhttp3 = new XMLHttpRequest();
+            xmlhttp3.open("get","reg_right.html",true);
+            xmlhttp3.onreadystatechange = function(){
+                if(xmlhttp3.readyState == 4 && xmlhttp3.status==200){
+                    modal_right.innerHTML = xmlhttp3.responseText;
+                    modal_left.style.animation = "modal_animation 0.8s";
+                    modal_right.style.animation = "modal_animation 0.9s";
+                    login_reg_svg.style.animation = "login_svg_animation 1s";
+                    modal_left.style.animationFillMode = "forwards";
+                    modal_right.style.animationFillMode = "forwards";
+                    login_reg_svg.style.animationFillMode = "forwards";
+                    $(':input').labelauty();
+                }
+            };
+            xmlhttp3.send(null);
+        }
+    };
+    xmlhttp2.send(null);
+}
+function login_show(){
+    modal_left.style.width = "60%";
+    modal_right.style.width = "40%";
+    var xmlhttp2 = new XMLHttpRequest();
+    xmlhttp2.open("get","login_left.html",true);
+    xmlhttp2.onreadystatechange = function(){
+        if(xmlhttp2.readyState == 4 && xmlhttp2.status==200){
+            modal_left.innerHTML = xmlhttp2.responseText;
+            var xmlhttp3 = new XMLHttpRequest();
+            xmlhttp3.open("get","login_right.html",true);
+            xmlhttp3.onreadystatechange = function(){
+                if(xmlhttp3.readyState == 4 && xmlhttp3.status==200){
+                    modal_right.innerHTML = xmlhttp3.responseText;
+                    modal_left.style.animation = "modal_animation 0.8s";
+                    modal_right.style.animation = "modal_animation 0.9s";
+                    login_reg_svg.style.animation = "login_svg_animation 1s";
+                    modal_left.style.animationFillMode = "forwards";
+                    modal_right.style.animationFillMode = "forwards";
+                    login_reg_svg.style.animationFillMode = "forwards";
+                }
+            };
+            xmlhttp3.send(null);
+        }
+    };
+    xmlhttp2.send(null);
+}
+function continue_animation(){
+    reg_right_div.style.animation = "continue_animation 0.8s";
+    reg_right_div_2.style.animation = "continue_animation 0.8s";
+    reg_right_div.style.animationFillMode = "forwards";
+    reg_right_div_2.style.animationFillMode = "forwards";
+    modal_left_h2_1.className = "";
+    modal_left_h2_2.className = "modal_left_shadow";
+    login_reg_svg.style.animation = "login_svg_close_animation 0.6s";
+}
+function change_reg(){
+    login_right_div.style.opacity = 0;
+    login_left_div_div.style.opacity = 0;
+    setTimeout(function(){
+        var xmlhttp2 = new XMLHttpRequest();
+        xmlhttp2.open("get","reg_left.html",true);
+        xmlhttp2.onreadystatechange = function(){
+            if(xmlhttp2.readyState == 4 && xmlhttp2.status==200){
+                modal_left.innerHTML = xmlhttp2.responseText;
+                login_reg_svg.style.animation = "change_login_svg_animation_2 0.4s";
+                login_reg_svg.style.animationFillMode = "forwards";
+                reg_left_div_div.style.animation = "show_modal_right_animation 0.4s";
+                var xmlhttp3 = new XMLHttpRequest();
+                xmlhttp3.open("get","reg_right.html",true);
+                xmlhttp3.onreadystatechange = function(){
+                    if(xmlhttp3.readyState == 4 && xmlhttp3.status==200){
+                        modal_right.innerHTML = xmlhttp3.responseText;
+                        reg_right_div.style.animation = "show_modal_right_animation 0.4s";
+                        reg_right_div_2.style.animation = "show_modal_right_animation 0.4s";
+                        $(':input').labelauty();
+                    }
+                };
+                xmlhttp3.send(null);
+            }
+        };
+        xmlhttp2.send(null);
+    },400);
+    modal_left.style.animation = "change_reg_modal_left_animation 0.8s";
+    modal_right.style.animation = "change_reg_modal_right_animation 0.8s";
+    modal_left.style.animationFillMode = "forwards";
+    modal_right.style.animationFillMode = "forwards";
+    setTimeout(function(){
+        login_reg_img.src = "svg/登陆账户.svg";
+    },800)
+    login_reg_svg.style.animation = "change_reg_svg_animation_1 0.8s";
+    login_reg_svg.style.animationFillMode = "forwards";
+}
+function change_login(){
+    reg_right_div.style.opacity = 0;
+    reg_right_div_2.style.opacity = 0;
+    reg_left_div_div.style.opacity = 0;
+    setTimeout(function(){
+        var xmlhttp2 = new XMLHttpRequest();
+        xmlhttp2.open("get","login_left.html",true);
+        xmlhttp2.onreadystatechange = function(){
+            if(xmlhttp2.readyState == 4 && xmlhttp2.status==200){
+                modal_left.innerHTML = xmlhttp2.responseText;
+                login_reg_svg.style.animation = "change_reg_svg_animation_2 0.4s";
+                login_reg_svg.style.animationFillMode = "forwards";
+                login_left_div_div.style.animation = "show_modal_right_animation 0.4s";
+                var xmlhttp3 = new XMLHttpRequest();
+                xmlhttp3.open("get","login_right.html",true);
+                xmlhttp3.onreadystatechange = function(){
+                    if(xmlhttp3.readyState == 4 && xmlhttp3.status==200){
+                        modal_right.innerHTML = xmlhttp3.responseText;
+                        login_right_div.style.animation = "show_modal_right_animation 0.4s";
+                        $(':input').labelauty();
+                    }
+                };
+                xmlhttp3.send(null);
+            }
+        };
+        xmlhttp2.send(null);
+    },400);
+    modal_left.style.animation = "change_login_modal_left_animation 0.8s";
+    modal_right.style.animation = "change_login_modal_right_animation 0.8s";
+    modal_left.style.animationFillMode = "forwards";
+    modal_right.style.animationFillMode = "forwards";
+    setTimeout(function(){
+        login_reg_img.src = "svg/注册账户.svg";
+    },800)
+    login_reg_svg.style.animation = "change_login_svg_animation_1 0.8s";
+    login_reg_svg.style.animationFillMode = "forwards";
+}
