7bfc0b40917b354794e35c403de2378b401d1ed1
sky-du <duminchao>
2017-03-16 15:07:40 +0800

    一月分数显示

--- vfast/vrecord/urls.py
+++ vfast/vrecord/urls.py
@@ -5,4 +5,5 @@ from vrecord import views
 urlpatterns = [
     url(r'^video', views.record_video, name='recordvideo'),
     url(r'^weekscore', views.get_score_seven_day, name='weekscore'),
+    url(r'^monthscore', views.get_score_thirty_day, name='monthscore'),
 ]
