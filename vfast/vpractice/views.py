4c299e2cac73a6f5bbd20f7bdc53c97a65c586b0
sky-du <duminchao>
2017-03-27 10:59:46 +0800

    关注功能

--- vfast/vpractice/views.py
+++ vfast/vpractice/views.py
@@ -52,7 +52,7 @@ def show_question(request):
         qid = request.GET.get('qid')
         question = Question.objects.get(id=qid)
         replays = Replay.objects.filter(question=question).values().order_by('-createtime')
-        attention = Attention.objects.filter(qid=qid, uid=uid).exists()             #用户是否关注问题
+        attention = Attention.objects.filter(qid=qid, uid=uid).exists()  # 用户是否关注问题
         qrcomment = QRcomment.objects.filter(uid=uid).values()
 
         qcomment = {}
@@ -73,7 +73,8 @@ def show_question(request):
                 else:
                     replay['status'] = False
 
-        print attention, qcomment, replays
+        # print attention, qcomment, replays
+        # qcomment 用户是否点赞, question 问题对象, relays 回复, attention 是否关注问题
         return render(request, 'detailsQA.html',
                       {'question': question, 'replays': replays, 'qcomment': qcomment, 'attention': attention})
     except:
@@ -107,7 +108,7 @@ def qr_comment(request):
     try:
         qid = request.GET.get('qid')
         rid = request.GET.get('rid')
-        status = request.GET.get('status')  # 1为赞, -1踩
+        status = request.GET.get('type')  # 1为赞, -1踩
         status = int(status)
         try:
             uid = request.session['user']['id']
@@ -139,18 +140,19 @@ def attention_question(request):
     """关注问题功能"""
     try:
         qid = request.GET.get('qid')
-        type = request.GET.get('attention')  # 1关注问题, 0取消关注
+        attention = request.GET.get('attention')  # 1关注问题, 0取消关注
         try:
             uid = request.session['user']['id']
         except:
             return False
         ret = Attention.objects.filter(qid=qid, uid=uid).exists()
-        if ret and type==0:
+        print qid, attention, ret, type(attention)
+        if ret and attention == '0':
             Attention.objects.filter(qid=qid, uid=uid).delete()
             return HttpResponse(json.dumps({'code': 0, 'msg': '取消关注成功'}))
-        elif not ret and type ==1:
+        elif ret == False and attention == '1':
             Attention.objects.create(qid=qid, uid=uid)
-            return HttpResponse(json.dumps({'code':0, 'msg':'关注成功'}))
+            return HttpResponse(json.dumps({'code': 0, 'msg': '关注成功'}))
         else:
             return HttpResponse(json.dumps({'code': 0, 'msg': 'attention_question , interface right'}))
     except:
