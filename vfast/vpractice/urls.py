569250d57e283cbdd916b6f84e21f79c75e89b1f
sky-du <duminchao>
2017-03-31 14:49:45 +0800

    fix bug vpractice/url.py 优先级匹配的问题

--- vfast/vpractice/urls.py
+++ vfast/vpractice/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls import url
 from vpractice import views
 
 urlpatterns = [
-    url(r'question', views.show_question, name='show_question'),
-    url(r'qrcomment', views.qr_comment),
-    url(r'attention', views.attention_question, name='attention_question'),
-    url(r'replay', views.add_replay, name='replay'),
-    url(r'update_question', views.update_question, name='update_question'),
-    url(r'update_replay',views.update_replay, name='update_replay'),
+    url(r'^question$', views.show_question, name='show_question'),
+    url(r'^qrcomment$', views.qr_comment),
+    url(r'^attention$', views.attention_question, name='attention_question'),
+    url(r'^replay$', views.add_replay, name='replay'),
+    url(r'^update_question$', views.update_question, name='update_question'),
+    url(r'^update_replay$',views.update_replay, name='update_replay'),
 ]
