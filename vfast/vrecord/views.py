2ba729fc329a5493b888f35cd86878317628039b
sky-du <duminchao>
2017-03-31 19:27:47 +0800

    fix get_tech_score  bug

--- vfast/vrecord/views.py
+++ vfast/vrecord/views.py
@@ -150,15 +150,3 @@ def get_score_thirty_day(request):
     except:
         logging.getLogger().error(traceback.format_exc())
         return HttpResponse('error')
-
-
-def sum_score_tech(uid):
-    try:
-        sql = 'select vp.name, vp.color, tmp.technology_id, tmp.score  from (select technology_id , sum(score) as score  from vrecord_score where user_id = %s group by technology_id) as tmp left join vcourse_program as vp on vp.id=tmp.technology_id;' % uid
-        result = dictfetchall(sql)
-        for item in result:
-            item['score'] = str(item['score'])
-        return result
-    except:
-        logging.getLogger().error(traceback.format_exc())
-        return HttpResponse('error')
