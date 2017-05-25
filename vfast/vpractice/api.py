6828b0e04017a45b391baf4261e95fa1a3e0b6d0
sky-du <duminchao>
2017-05-25 19:11:10 +0800

    排行榜数据添加排行

--- vfast/vpractice/api.py
+++ vfast/vpractice/api.py
@@ -7,14 +7,27 @@ def rank_front(rank_result, userid):
             position = rank_result.index(user)
     if position < 9:
         topten = rank_result[:10]
+        rank = 1
+        for i in topten:
+            i['position'] = rank
+            rank += 1
     else:
         topten = rank_result[:10]
-        topten.append(rank_result[position - 1])
-        topten.append(rank_result[position])
+        rank = 1
+        for i in topten:
+            i['position'] = rank
+            rank += 1
+        front = rank_result[position - 1]
+        front['position'] = position -1
+        current = rank_result[position]
+        current['postion'] = position
         try:
-            topten.append(rank_result[position + 1])
-        except IndexError:
+            behind = rank_result[position + 1]
+            behind['postion'] = position + 1
+            topten.append(front, current,behind)
+        except:
             pass
+        topten.append(front, current)
     for item in topten:
         if item.has_key('repatation'):
             item['repatation'] = str(item['repatation'])
