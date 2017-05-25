eae62634e6448caa90e9d06144be470a025cf611
Peng <542429405@qq.com>
2017-05-25 11:48:14 +0800

    '111'

--- vfast/vfast/views.py
+++ vfast/vfast/views.py
@@ -25,7 +25,7 @@ def test(request):
     # course = connection.cursor()
     # course.execute('select * from vcourse_video')
     # a = dictfetchall(cursor=course)
-    return render(request, "Community.html")
+    return render(request, "rankingList.html")
 
 
 def logout(request):
