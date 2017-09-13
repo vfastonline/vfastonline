3bb4c305ff51e7841e2dea75d73ad751ede4c672
sky-du <duminchao>
2017-09-13 10:47:10 +0800

    seven_day

--- vfast/vrecord/views.py
+++ vfast/vrecord/views.py
@@ -176,28 +176,23 @@ def record_video(request):
 
 def get_score_seven_day(request):
     try:
-        # uid = request.session['user']['id']
-        uid = request.GET.get('uid', None)
+        try:
+            uid = request.session['user']['id']
+        except:
+            uid = request.GET.get('uid', None)
         if not uid:
-            return HttpResponse(json.dumps({'code':1, 'msg':'parameter error!'}))
-        seven_day = []
+            return HttpResponse(json.dumps({'code': 1, 'msg': 'parameter error!'}))
         date, score, vtime = [], [], []
         for i in range(-7, 1):
-            tmp = {}
-            tmp['date'] = str(get_day_of_day(i))
-            tmp['label'] = str(get_day_of_day(i))
             date.append(str(get_day_of_day(i)))
             # 获取最近七天的得分
             sql_score = """select sum(score) as score from vrecord_score where createtime = '%s' and user_id = %s""" % (
                 str(get_day_of_day(i)), uid)
             ret = dictfetchall(sql_score)
             if ret[0]['score'] is None:
-                tmp['value'] = 0
                 score.append(0)
             else:
-                tmp['value'] = int(ret[0]['score'])
                 score.append(int(ret[0]['score']))
-            seven_day.append(tmp)
             # 获取最近七天的观看时长
             sql_vtime = """select * from vrecord_watchtime where userid=%s and createtime='%s'; """ % (
                 uid, str(get_day_of_day(i)))
@@ -207,7 +202,7 @@ def get_score_seven_day(request):
             else:
                 vtime.append(0)
         result = {'date': date, 'score': score, 'vtime': vtime}
-        return HttpResponse(json.dumps({'weekscore': seven_day, 'result': result}, ensure_ascii=False))
+        return HttpResponse(json.dumps({'result': result}, ensure_ascii=False))
     except:
         logging.getLogger().error(traceback.format_exc())
         return HttpResponse(json.dumps({'code': 128}, ensure_ascii=False))
