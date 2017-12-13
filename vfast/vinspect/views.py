ff99ae343dd93ef296a9e1b94fbd374632f52f6b
sky-du <duminchao>
2017-12-13 11:30:48 +0800

    防止用户对问卷调查多次点击

--- vfast/vinspect/views.py
+++ vfast/vinspect/views.py
@@ -60,20 +60,21 @@ def inspect_result(request):
             data.pop('opinion')
             data.pop('inspectid')
             inspect = Inspect.objects.get(id=inspectid)
-
+            if  InspectResult.objects.filter(user=user, inspect=inspect, opinion__isnull=False).exists():
+                return HttpResponse(json.dumps({'code': 1, 'msg':'你之前已经评论过~!'},ensure_ascii=False))
             for k, v in data.iteritems():
                 inspectoption = InspectOption.objects.get(id=int(k))
                 InspectResult.objects.create(user=user, inspect=inspect, option=v, inspectoption=inspectoption)
 
             InspectResult.objects.create(user=user, inspect=inspect, opinion=opinion)
 
-            return HttpResponse(json.dumps({'code':0}))
+            return HttpResponse(json.dumps({'code':0, 'msg':'感谢您的参与,评论成功'}, ensure_ascii=False))
         else:
-            return HttpResponse({'code':1})
+            return HttpResponse(json.dumps({'code':1, 'msg':'请使用POST方法!'}, ensure_ascii=False))
 
     except:
         logging.getLogger().error(traceback.format_exc())
-        return HttpResponse(json.dumps({'code':128}))
+        return HttpResponse(json.dumps({'code':128, 'msg':'服务器错误'},ensure_ascii=False))
 
 
 
