65c41d6c85f7921bc2810fbd4885643e3490548e
xuhuiliang <15011544971@163.com>
2018-08-27 16:35:54 +0800

    test install conda

--- setup-all.py
+++ setup-all.py
@@ -2,15 +2,15 @@
 import os
 
 
+def install_conda():
+	os.system("yum install anaconda")
+
+
 def tar_vfast():
 	vfast_dir = "/usr/local/vfastonline/vfast"
 	os.system("python vfast/setup.py sdist")
 	os.system("mkdir -p %s && tar -zxvf vfast-1.0.tar.gz -C %s --strip-components 1" % (vfast_dir, vfast_dir))
 	os.system("mv media %s" % vfast_dir)
-
-
-def install_conda():
-	os.system("sh Anaconda2-5.2.0-Linux-x86_64.sh")
 
 
 def make_conda_env():
@@ -50,13 +50,14 @@ def mysql_dump():
 
 
 def install_all():
-	tar_vfast()
-	# install_conda()
+	# tar_vfast()
+	install_conda()
 	# make_conda_env()
 	# install_vfast_setup()
 	# install_uwsgi()
 	# install_nginx()
 	# mv_uwsgi_nginx_config()
+	pass
 
 
 if __name__ == "__main__":
