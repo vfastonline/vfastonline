43918abdd65a47d7479d448c767e48d94c974799
xuhuiliang <15011544971@163.com>
2018-08-27 10:57:49 +0800

    mv setup-all

--- setup-all.py
+++ setup-all.py
@@ -4,6 +4,7 @@ import os
 
 def tar_vfast():
 	vfast_dir = "/usr/local/vfastonline/vfast"
+	os.system("python vfast setup.py sdist")
 	os.system("mkdir -p %s && tar -zxvf vfast-1.0.tar.gz -C %s --strip-components 1" % (vfast_dir, vfast_dir))
 	os.system("mv media %s" % vfast_dir)
 
@@ -50,12 +51,12 @@ def mysql_dump():
 
 def install_all():
 	tar_vfast()
-	install_conda()
-	make_conda_env()
-	install_vfast_setup()
-	install_uwsgi()
-	install_nginx()
-	mv_uwsgi_nginx_config()
+	# install_conda()
+	# make_conda_env()
+	# install_vfast_setup()
+	# install_uwsgi()
+	# install_nginx()
+	# mv_uwsgi_nginx_config()
 
 
 if __name__ == "__main__":
