a6592ee19b47fe2447db6950fbacc2a08b7c0af5
xuhuiliang <15011544971@163.com>
2018-08-30 15:13:31 +0800

    add PYTHON_EGG_CACHE env

--- vfast/vfast/wsgi.py
+++ vfast/vfast/wsgi.py
@@ -9,12 +9,14 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 
 import os
 import sys
+
 reload(sys)
 
 sys.setdefaultencoding('utf-8')
 
 from django.core.wsgi import get_wsgi_application
 
+os.environ['PYTHON_EGG_CACHE'] = '/tmp/.python-eggs'
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vfast.settings")
 
 application = get_wsgi_application()
